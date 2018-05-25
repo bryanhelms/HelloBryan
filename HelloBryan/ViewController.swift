59291eb8e031bc342b58d96ab4561b8907f9e12c
Bryan Helms <bryan.helms@microsoft.com>
2018-05-24 20:47:49 -0700

    Initial Commit

---
+++ HelloBryan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloBryan
+//
+//  Created by Bryan Helms on 5/24/18.
+//  Copyright © 2018 Bryan Helms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+ override func viewDidLoad() {
+  super.viewDidLoad()
+  // Do any additional setup after loading the view, typically from a nib.
+ }
+
+ override func didReceiveMemoryWarning() {
+  super.didReceiveMemoryWarning()
+  // Dispose of any resources that can be recreated.
+ }
+
+
+}
+
